55e74a43c54f47dbaf92fe28115488dee16c4ae1
DongWook-Kwak <kdw381@naver.com>
2026-07-23 18:09:10 +0900

    feat : 초기설정

---
+++ src/main/java/com/mixmate/MixmateWasApplication.java
@@ -0,0 +1,13 @@
+package com.mixmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixmateWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixmateWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mixmate/MixmateWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixmateWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
