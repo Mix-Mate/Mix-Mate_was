8ffc80ffa6d59457490c4942680ea2eba15950ae
meoooogus <kdh031230@gmail.com>
2026-08-06 16:29:31 +0900

    feat : Group, GroupStatus 엔티티 추가
    - 테이블명은 예약어를 피해 event_group
    - status는 EnumType.STRING, inviteCode는 unique + length 8
    - createdAt은 @CreatedDate로 채움

--- src/main/java/com/mixmate/domain/group/entity/Group.java
+++ src/main/java/com/mixmate/domain/group/entity/Group.java
@@ -1,4 +1,39 @@
 package com.mixmate.domain.group.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "event_group")
+@EntityListeners(AuditingEntityListener.class)
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private GroupStatus status;
+
+    @Column(nullable = false, unique = true, length = 8)
+    private String inviteCode;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/mixmate/domain/group/entity/GroupStatus.java
+++ src/main/java/com/mixmate/domain/group/entity/GroupStatus.java
@@ -1,4 +1,6 @@
-package com.mixmate.domain.auth.entity;
+package com.mixmate.domain.group.entity;
 
 public enum GroupStatus {
+    BEFORE_FIRST_ASSIGNMENT, FIRST_ROUND, VOTING,
+    BEFORE_SECOND_ASSIGNMENT, SECOND_ROUND, FINISHED
 }
