0cd15152d8c500502b27df755d545c7b7283b6c8
meoooogus <kdh031230@gmail.com>
2026-08-07 13:29:51 +0900

    feat : ParticipantRepository 추가
    그룹 참가자 목록 조회, 입장 중복 검사, 그룹 내 본인 조회.

---
+++ src/main/java/com/mixmate/domain/participant/repository/ParticipantRepository.java
@@ -0,0 +1,18 @@
+package com.mixmate.domain.participant.repository;
+
+import com.mixmate.domain.auth.entity.User;
+import com.mixmate.domain.group.entity.Group;
+import com.mixmate.domain.participant.entity.Participant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+
+    List<Participant> findByGroup(Group group);
+
+    boolean existsByGroupAndUser(Group group, User user);
+
+    Optional<Participant> findByGroupAndUser(Group group, User user);
+}
