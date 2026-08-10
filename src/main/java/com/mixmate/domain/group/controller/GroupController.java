1a89551a0c57ed341bed41f69798b4c3a751bf3a
meoooogus <kdh031230@gmail.com>
2026-08-10 14:11:18 +0900

    feat : 그룹 생성 API 컨트롤러 추가

---
+++ src/main/java/com/mixmate/domain/group/controller/GroupController.java
@@ -0,0 +1,35 @@
+package com.mixmate.domain.group.controller;
+
+import com.mixmate.domain.group.dto.GroupCreateRequest;
+import com.mixmate.domain.group.dto.GroupCreateResponse;
+import com.mixmate.domain.group.service.GroupService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/groups")
+public class GroupController {
+
+    private final GroupService groupService;
+
+    @PostMapping
+    public ResponseEntity<GroupCreateResponse> createGroup(
+            @Valid @RequestBody GroupCreateRequest dto,
+            @RequestHeader("X-User-Id") Long userId
+            // TODO: auth 머지되면 @AuthenticationPrincipal CustomUserDetails userDetails로 교체
+            ) {
+        GroupCreateResponse response = groupService.createGroup(dto, userId);
+        return ResponseEntity
+                .created(URI.create("/api/v1/groups/" + response.groupId()))
+                .body(response);
+    }
+}
