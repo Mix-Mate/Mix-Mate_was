734f7a6a1ca21a9bdb71b211b18c968e28c066ab
meoooogus <kdh031230@gmail.com>
2026-08-11 01:05:56 +0900

    feat : 그룹 수정 요청 DTO 추가

---
+++ src/main/java/com/mixmate/domain/group/dto/GroupUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.mixmate.domain.group.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record GroupUpdateRequest(
+        @NotBlank(message = "그룹 이름을 입력해주세요.")
+        String groupName,
+
+        String description
+) {
+
+}
