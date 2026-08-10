203b7d633d1d1e01c96b102f89aa36606c806d48
DongWook-Kwak <kdw381@naver.com>
2026-08-11 01:34:50 +0900

    feat : swaggerconfig 구현

---
+++ src/main/java/com/mixmate/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.mixmate.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String JWT_SCHEME_NAME = "JWT";
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(JWT_SCHEME_NAME, jwtScheme));
+    }
+}
