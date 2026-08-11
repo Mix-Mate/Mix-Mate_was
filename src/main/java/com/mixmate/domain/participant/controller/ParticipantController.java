ef56f964cb88cbfc756964bec4175660c7f37119
meoooogus <kdh031230@gmail.com>
2026-08-11 10:16:02 +0900

    refact : 로그인 사용자를 X-User-Id 헤더 대신 @AuthenticationPrincipal로 수령

--- src/main/java/com/mixmate/domain/participant/controller/ParticipantController.java
+++ src/main/java/com/mixmate/domain/participant/controller/ParticipantController.java
@@ -4,12 +4,13 @@ import com.mixmate.domain.participant.dto.ParticipantListResponse;
 import com.mixmate.domain.participant.dto.ParticipantProfileResponse;
 import com.mixmate.domain.participant.enums.Round;
 import com.mixmate.domain.participant.service.ParticipantService;
+import com.mixmate.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,19 +27,17 @@ public class ParticipantController {
     public ResponseEntity<ParticipantListResponse> getParticipants(
             @PathVariable Long groupId,
             @RequestParam Round round,
-            @RequestHeader("X-User-Id") Long userId
-            // TODO: auth 머지되면 @AuthenticationPrincipal CustomUserDetails userDetails로 교체
+            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        return ResponseEntity.ok(participantService.getParticipants(groupId, round, userId));
+        return ResponseEntity.ok(participantService.getParticipants(groupId, round, userDetails.getUserId()));
     }
 
     @GetMapping("/{groupId}/participants/{participantId}")
     public ResponseEntity<ParticipantProfileResponse> getParticipantProfile(
             @PathVariable Long groupId,
             @PathVariable Long participantId,
-            @RequestHeader("X-User-Id") Long userId
-            // TODO: auth 머지되면 @AuthenticationPrincipal CustomUserDetails userDetails로 교체
+            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        return ResponseEntity.ok(participantService.getParticipantProfile(groupId, participantId, userId));
+        return ResponseEntity.ok(participantService.getParticipantProfile(groupId, participantId, userDetails.getUserId()));
     }
 }
