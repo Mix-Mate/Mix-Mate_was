67d9b638a3b7f7b35e525ff586742f2215f2ffc8
meoooogus <kdh031230@gmail.com>
2026-08-11 12:18:04 +0900

    feat : 그룹 참가자 일괄 삭제 쿼리 추가

--- src/main/java/com/mixmate/domain/participant/repository/ParticipantRepository.java
+++ src/main/java/com/mixmate/domain/participant/repository/ParticipantRepository.java
@@ -6,6 +6,9 @@ import com.mixmate.domain.participant.dto.ParticipantProfileDetail;
 import com.mixmate.domain.participant.entity.Participant;
 import com.mixmate.domain.participant.enums.RoundParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,4 +24,8 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
     List<Participant> findByGroupAndRoundParticipation(Group group, RoundParticipation roundParticipation);
 
     Optional<Participant> findByParticipantIdAndGroup(Long participantId, Group group);
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM Participant p WHERE p.group = :group")
+    void deleteAllByGroup(@Param("group") Group group);
 }
