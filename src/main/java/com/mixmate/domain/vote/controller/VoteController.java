92bdd4d262636df4c0ebda2476c10c2e457f9e55
DongWook-Kwak <kdw381@naver.com>
2026-08-14 01:07:58 +0900

    Feat :   투표 통합 controller 구현
    - MVP 투표
    
    - 2차 투표
    
    - 2차 투표 현황확인
    
    - 투표결과 확인(MVP)
    
    - 전체 투표 종료

---
+++ src/main/java/com/mixmate/domain/vote/controller/VoteController.java
@@ -0,0 +1,62 @@
+package com.mixmate.domain.vote.controller;
+
+import com.mixmate.domain.vote.api.VoteApi;
+import com.mixmate.domain.vote.dto.request.MvpVoteReqDto;
+import com.mixmate.domain.vote.dto.request.Round2VoteReqDto;
+import com.mixmate.domain.vote.dto.response.Round2VoteStatusResDto;
+import com.mixmate.domain.vote.dto.response.VoteResultResDto;
+import com.mixmate.domain.vote.service.VoteService;
+import com.mixmate.security.CustomUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/groups")
+public class VoteController implements VoteApi {
+
+    private final VoteService voteService;
+
+    public ResponseEntity<Void> voteMvp(
+            @PathVariable Long groupId,
+            @Valid @RequestBody MvpVoteReqDto dto,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        voteService.voteMvp(dto, groupId, userDetails.getUserId());
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<Void> voteSecondRound(
+            @PathVariable Long groupId,
+            @Valid @RequestBody Round2VoteReqDto dto,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        voteService.voteSecondRound(dto, groupId, userDetails.getUserId());
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<Round2VoteStatusResDto> getSecondRoundVoteStatus(
+            @PathVariable Long groupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        return ResponseEntity.ok(voteService.getSecondRoundVoteStatus(groupId, userDetails.getUserId()));
+    }
+
+    public ResponseEntity<VoteResultResDto> getVoteResult(
+            @PathVariable Long groupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        return ResponseEntity.ok(voteService.getVoteResult(groupId, userDetails.getUserId()));
+    }
+
+    public ResponseEntity<Void> finishVote(
+            @PathVariable Long groupId,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        voteService.finishVote(groupId, userDetails.getUserId());
+        return ResponseEntity.noContent().build();
+    }
+}
