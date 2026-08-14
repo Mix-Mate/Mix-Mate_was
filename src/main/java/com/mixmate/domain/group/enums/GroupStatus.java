05af1e146f7028b65190c79358e0f619c7fb2b1d
meoooogus <kdh031230@gmail.com>
2026-08-14 10:47:52 +0900

    feat : 투표 종료 상태 추가

--- src/main/java/com/mixmate/domain/group/enums/GroupStatus.java
+++ src/main/java/com/mixmate/domain/group/enums/GroupStatus.java
@@ -1,13 +1,21 @@
 package com.mixmate.domain.group.enums;
 
 public enum GroupStatus {
-    BEFORE_FIRST_ASSIGNMENT, FIRST_ROUND, VOTING,
+    BEFORE_FIRST_ASSIGNMENT, FIRST_ROUND, VOTING, VOTE_CLOSED,
     BEFORE_SECOND_ASSIGNMENT, SECOND_ROUND, FINISHED;
 
     public boolean isSecondRoundAssigned() {
         return switch (this) {
             case BEFORE_SECOND_ASSIGNMENT, SECOND_ROUND, FINISHED -> true;
-            case BEFORE_FIRST_ASSIGNMENT, FIRST_ROUND, VOTING -> false;
+            case BEFORE_FIRST_ASSIGNMENT, FIRST_ROUND, VOTING, VOTE_CLOSED -> false;
+        };
+    }
+
+    // finished 상태에서 멱등으로 처리할 때는 서비스레이어 조기 반환으로 처리
+    public boolean canFinish() {
+        return switch (this) {
+            case VOTE_CLOSED, SECOND_ROUND -> true;
+            case BEFORE_FIRST_ASSIGNMENT, FIRST_ROUND, VOTING, BEFORE_SECOND_ASSIGNMENT, FINISHED -> false;
         };
     }
 }
