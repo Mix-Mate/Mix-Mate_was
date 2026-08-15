d3eaeb5a374c2d9598baf029d0099c8e954a1bc6
meoooogus <kdh031230@gmail.com>
2026-08-16 03:09:23 +0900

    feat : 조 편성 조건 enum 추가

---
+++ src/main/java/com/mixmate/domain/assignment/enums/AssignmentCondition.java
@@ -0,0 +1,11 @@
+package com.mixmate.domain.assignment.enums;
+
+public enum AssignmentCondition {
+    GENDER_BALANCE,
+    MBTI_BALANCE,
+    GRADE_SPREAD,
+    MAJOR_SPREAD,
+    NEWCOMER_SPREAD,
+    POSITION_SPREAD,
+    SIZE_BALANCE
+}
--- src/main/java/com/mixmate/domain/participant/enums/Mbti.java
+++ src/main/java/com/mixmate/domain/participant/enums/Mbti.java
@@ -2,7 +2,11 @@ package com.mixmate.domain.participant.enums;
 
 public enum Mbti {
     ISTP, ISTJ, ISFP, ISFJ, INTP, INTJ, INFP, INFJ,
-    ESTP, ESTJ, ESFP, ESFJ, ENTP, ENTJ, ENFP, ENFJ
+    ESTP, ESTJ, ESFP, ESFJ, ENTP, ENTJ, ENFP, ENFJ;
+
+    public boolean isExtrovert() {
+        return this.name().charAt(0) == 'E';
+    }
 }
 // I E
 // S N
